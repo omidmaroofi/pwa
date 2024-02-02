172b6b9742a613f6ff48db30bbb588ec78d5a8de
Omid <34368378+omidmaroofi@users.noreply.github.com>
2024-02-02 15:13:09 +0330

    Add files via upload

---
+++ public/static/js/app.js
@@ -0,0 +1,38 @@
+// install service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      // console.log("Service worker registration succeeded:", registration);
+    })
+    .catch((err) => {
+      // console.log("Service worker registration failed:", err);
+    });
+}
+
+// install banner
+let installPromptEvent;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  console.log("before install prompt event");
+  installPromptEvent = e;
+});
+
+document.querySelector(".fixed-action-btn a").addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(installPromptEvent);
+  if (installPromptEvent) {
+    installPromptEvent.prompt();
+
+    installPromptEvent.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User Accepted");
+      } else {
+        console.log("User dismissed");
+      }
+
+      installPromptEvent = null;
+    });
+  }
+});
