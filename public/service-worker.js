7ccb1f27b017268c642ac29f94e471f6f7cffec4
Omid <34368378+omidmaroofi@users.noreply.github.com>
2024-02-02 22:45:57 +0330

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,33 @@
+let CACHE_VERSION = 1.1;
+let CURRENT_CACHE = {
+  static: "static-cache-v" + CACHE_VERSION,
+};
+
 self.addEventListener("install", (event) => {
   console.log("installing service worker", event);
   event.waitUntil(
-    caches.open("front-cache").then((cache) => {
-      cache.add("/static/css/materialize.min.css");
+    caches.open(CURRENT_CACHE["static"]).then((cache) => {
+      cache.addAll([
+        "/",
+        "/static/css/materialize.min.css",
+        "/static/js/app.js",
+        "/static/js/materialize.min.js",
+        "/static/css/vazir.css",
+        "/static/css/style.css",
+      ]);
     })
   );
 });
 
 self.addEventListener("activate", (event) => {
   console.log("activating service worker", event);
-  console.log("v1");
+  let expectedCacheName = Object.values(CURRENT_CACHE);
+  console.log(expectedCacheName);
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.open("front-cache").then((cache) => {
+    caches.open(CURRENT_CACHE["static"]).then((cache) => {
       return cache.match(event.request).then((response) => {
         return response || fetch(event.request);
       });
